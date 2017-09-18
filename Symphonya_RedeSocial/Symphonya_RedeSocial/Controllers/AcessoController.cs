60182022f4fb002044d9cc9b3408b8df25232b4c
Victor Rodrigues <victorluizrodrigues@gmail.com>
2017-09-18 17:32:38 -0300

    Adicionando sistema de login.

--- Symphonya_RedeSocial/Symphonya_RedeSocial/Controllers/AcessoController.cs
+++ Symphonya_RedeSocial/Symphonya_RedeSocial/Controllers/AcessoController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Symphonya_RedeSocial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Symphonya_RedeSocial.Controllers
 {
@@ -11,12 +13,43 @@ namespace Symphonya_RedeSocial.Controllers
         // GET: Acesso
         public ActionResult Login()
         {
-            return View();
-        }
+
+                if (Request.HttpMethod == "POST")
+                {
+                    String Email = Request.Form["email"].ToString();
+                    String Senha = Request.Form["senha"].ToString();
+
+                    //CRIA CRIPTOGRAFIA DA SENHA DO USUARIO
+                    String SenhaEncriptada = FormsAuthentication.HashPasswordForStoringInConfigFile(Senha, "SHA1");
+
+                //AUTENTICA USUARIO
+                if (Usuario.Autenticar(Email, SenhaEncriptada))
+                {
+                    Usuario Usuario = new Usuario(Email, SenhaEncriptada);
+                    Session["Usuario"] = Usuario;
+                    Response.Redirect("/Menu/Feed");
+                }
+
+                    //CASO INFORMACOES DIGITADAS SEJAM INVALIDAS
+                    ViewBag.MsgErro = "Usuário e/ou Senha incorretos!";
+
+                }
+                
+                return View();
+            }
 
         public ActionResult Cadastro()
         {
             return View();
         }
+
+        public void Sair()
+        {
+            Session.Abandon();
+            Session.Clear();
+
+            Response.Redirect("/Acesso/Login", false);
+        }
+
     }
 }
---
+++ Symphonya_RedeSocial/Symphonya_RedeSocial/Models/Usuario.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Symphonya_RedeSocial.Models
+{
+    public class Usuario
+    {
+        public Int32 ID { get; set; }
+        public String Nome { get; set; }
+        public String Sobrenome { get; set; }
+        public DateTime Nascimento { get; set; }
+        public Boolean Sexo { get; set; }
+        public String Imagem_Perfil { get; set; }
+        public String Email { get; set; }
+        public String Senha { get; set; }
+        public String Cidade { get; set; }
+        public String Estado { get; set; }
+        public int Avaliacao { get; set; }
+        public Boolean Modo { get; set; }
+
+        public Usuario() { }
+
+        public Usuario(Int32 ID)
+        {
+            SqlConnection Conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["Symphonya"].ConnectionString);
+            Conexao.Open();
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = Conexao;
+            Comando.CommandText = "SELECT * FROM Usuario WHERE ID=@ID;";
+            Comando.Parameters.AddWithValue("@ID", ID);
+
+            SqlDataReader Leitor = Comando.ExecuteReader();
+
+            Leitor.Read();
+
+            this.ID = (Int32)Leitor["ID"];
+            this.Nome = (String)Leitor["Nome"];
+            this.Sobrenome = (String)Leitor["Sobrenome"];
+            this.Nascimento = (DateTime)Leitor["Nascimento"];
+            this.Sexo = (Boolean)Leitor["Sexo"];
+            this.Imagem_Perfil = (String)Leitor["Imagem_Perfil"];
+            this.Email = (String)Leitor["Email"];
+            this.Senha = (String)Leitor["Senha"];
+            this.Cidade = (String)Leitor["Cidade"];
+            this.Estado = (String)Leitor["Estado"];
+            this.Avaliacao = (Int32)Leitor["Avaliacao"];
+            this.Modo = (Boolean)Leitor["Modo"];
+
+            Conexao.Close();
+        }
+        public Usuario(String Email, String Senha)
+        {
+            SqlConnection Conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["Symphonya"].ConnectionString);
+            Conexao.Open();
+
+            //CRIACAO DO COMANDO SQL
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = Conexao;
+            Comando.CommandText = "SELECT * FROM Usuario WHERE Email=@Email AND Senha=@Senha;";
+            Comando.Parameters.AddWithValue("@Email", Email);
+            Comando.Parameters.AddWithValue("@Senha", Senha);
+
+            SqlDataReader Leitor = Comando.ExecuteReader();
+
+            //LEITURA DO RESULTADO DO COMANDO
+            Leitor.Read();
+
+            //COLETA DE DADOS -> USUARIO
+            this.ID = (Int32)Leitor["ID"];
+            this.Nome = (String)Leitor["Nome"];
+            this.Sobrenome = (String)Leitor["Sobrenome"];
+            this.Nascimento = (DateTime)Leitor["Nascimento"];
+            this.Sexo = (Boolean)Leitor["Sexo"];
+            this.Imagem_Perfil = (String)Leitor["Imagem_Perfil"];
+            this.Email = (String)Leitor["Email"];
+            this.Senha = (String)Leitor["Senha"];
+            this.Cidade = (String)Leitor["Cidade"];
+            this.Estado = (String)Leitor["Estado"];
+            this.Avaliacao = (Int32)Leitor["Avaliacao"];
+            this.Modo = (Boolean)Leitor["Modo"];
+
+
+            Conexao.Close();
+        }
+        public Boolean NovoUser()
+        {
+
+            SqlConnection Conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["Symphonya"].ConnectionString);
+            Conexao.Open();
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = Conexao;
+            Comando.CommandText = "INSERT INTO Usuario (Nome, Sobrenome, Nascimento, Sexo, Imagem_Perfil, Email, Senha, Cidade, Estado, Avaliacao, Modo)"
+              + "VALUES (@Nome, @Sobrenome, @Nascimento, @Sexo, @Imagem_Perfil, @Email, @Senha, @Cidade, @Estado, @Avaliacao, @Modo);";
+            Comando.Parameters.AddWithValue("@Nome", this.Nome);
+            Comando.Parameters.AddWithValue("@Sobrenome", this.Sobrenome);
+            Comando.Parameters.AddWithValue("@Nascimento", this.Nascimento);
+            Comando.Parameters.AddWithValue("@Sexo", this.Sexo);
+            Comando.Parameters.AddWithValue("@Imagem_Perfil", this.Imagem_Perfil); //ALTERAR PARA IMAGEM PADRAO
+            Comando.Parameters.AddWithValue("@Email", this.Email);
+            Comando.Parameters.AddWithValue("@Senha", this.Senha);
+            Comando.Parameters.AddWithValue("@Cidade", this.Cidade);
+            Comando.Parameters.AddWithValue("@Estado", this.Estado);
+            Comando.Parameters.AddWithValue("@Avaliacao", 0); //USUARIO COM NENHUMA AVALIACAO -> 0
+            Comando.Parameters.AddWithValue("@Modo", 0); //USUARIO ENTRA OFFLINE, OU SEJA, NAO FAZ STREAM -> 0
+            
+            Int32 Resultado = Comando.ExecuteNonQuery();
+
+            Conexao.Close();
+
+            return Resultado > 0 ? true : false;
+        }
+        public Boolean NovaBio()
+        {
+            SqlConnection Conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["KatiauBD"].ConnectionString);
+            Conexao.Open();
+
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = Conexao;
+            Comando.CommandText = "UPDATE Usuario SET BioU = @Bio , NickU = @Nick, ImagemU = @Imagem WHERE ID = @ID;";
+            Comando.Parameters.AddWithValue("@ID", this.ID);
+            //Comando.Parameters.AddWithValue("@Nick", this.Nick);
+            //.Parameters.AddWithValue("@Bio", this.Bio);
+            //.Parameters.AddWithValue("@Imagem", this.ImagemPerfil);
+
+            Int32 Resultado = Comando.ExecuteNonQuery();
+
+            Conexao.Close();
+
+            return Resultado > 0 ? true : false;
+        }
+
+        public Boolean Novo()
+        {
+            SqlConnection Conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["KatiauBD"].ConnectionString);
+            Conexao.Open();
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = Conexao;
+            Comando.CommandText = "INSERT INTO Usuario (EmailU, NickU, NomeU, SobrenomeU, SenhaU, NascimentoU, BioU, ImagemU, Administrador)"
+              + "VALUES (@Email, @NickU, @Nome, @Sobrenome, @Senha, @Nascimento, @Bio, @ImagemPerfil, @Adm);";
+            Comando.Parameters.AddWithValue("@Email", this.Email);
+           // Comando.Parameters.AddWithValue("@NickU", this.Nick);
+            Comando.Parameters.AddWithValue("@Nome", this.Nome);
+            Comando.Parameters.AddWithValue("@Sobrenome", this.Sobrenome);
+            Comando.Parameters.AddWithValue("@Senha", this.Senha);
+            Comando.Parameters.AddWithValue("@Nascimento", this.Nascimento);
+            Comando.Parameters.AddWithValue("@Bio", "Biografia");
+            Comando.Parameters.AddWithValue("@ImagemPerfil", "tr4.jpg");
+            Comando.Parameters.AddWithValue("@Adm", 0);
+
+
+
+            Int32 Resultado = Comando.ExecuteNonQuery();
+
+            Conexao.Close();
+
+            return Resultado > 0 ? true : false;
+        }
+
+        public static List<Usuario> ListarU()
+        {
+            SqlConnection Conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["KatiauBD"].ConnectionString);
+            Conexao.Open();
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = Conexao;
+            Comando.CommandText = "SELECT * FROM Usuario;";
+
+            SqlDataReader Leitor = Comando.ExecuteReader();
+
+            List<Usuario> Users = new List<Usuario>();
+            while (Leitor.Read())
+            {
+                Usuario U = new Usuario();
+                U.ID = (Int32)Leitor["ID"];
+                U.Nome = ((String)Leitor["NomeU"]);
+              //  U.Nick = Leitor["NickU"].ToString();
+                U.Sobrenome = (String)Leitor["SobrenomeU"];
+                U.Email = ((String)Leitor["EmailU"]);
+                U.Senha = (String)Leitor["SenhaU"];
+              //  U.Nascimento = (String)Leitor["NascimentoU"];
+             //   U.Bio = (String)Leitor["BioU"];
+                //U.ImagemPerfil = (String)Leitor["ImagemU"];
+               // U.Adm = (Boolean)Leitor["Administrador"];
+
+
+                Users.Add(U);
+            }
+
+            Conexao.Close();
+
+            return Users;
+        }
+
+
+        public Boolean Apagar()
+        {
+            SqlConnection Conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["Symphonya"].ConnectionString);
+            Conexao.Open();
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = Conexao;
+            Comando.CommandText = "DELETE FROM Usuario WHERE ID = @ID;";
+            Comando.Parameters.AddWithValue("@ID", this.ID);
+
+            Int32 Resultado = Comando.ExecuteNonQuery();
+
+
+
+            return Resultado > 0 ? true : false;
+        }
+
+        public static Boolean Autenticar(String Email, String Senha)
+        {
+            SqlConnection Conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["Symphonya"].ConnectionString);
+
+            Conexao.Open();
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = Conexao;
+            Comando.CommandText = "SELECT ID FROM Usuario WHERE Email=@Email AND Senha=@Senha;";
+            Comando.Parameters.AddWithValue("@Email", Email);
+            Comando.Parameters.AddWithValue("@Senha", Senha);
+
+            SqlDataReader Leitor = Comando.ExecuteReader();
+
+            Leitor.Read();
+
+            //VERIFICA SE O LEITOR TROUXE RESULTADOS
+            if (!Leitor.HasRows)
+            {
+                //FECHA CONEXAO COM O BANCO
+                Conexao.Close();
+                return false;
+            }
+
+            //FECHA CONEXAO COM O BANCO
+            Conexao.Close();
+            return true;
+        }
+    }
+}
