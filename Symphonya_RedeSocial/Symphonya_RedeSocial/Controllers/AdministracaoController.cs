a92f8bf688bea56810785760021202f142222059
Victor Rodrigues <victorluizrodrigues@gmail.com>
2017-11-17 17:32:04 -0200

    Upload de arquivos.

---
+++ Symphonya_RedeSocial/Symphonya_RedeSocial/Controllers/AdministracaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Symphonya_RedeSocial.Controllers
+{
+    public class AdministracaoController : Controller
+    {
+        // GET: Administracao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
