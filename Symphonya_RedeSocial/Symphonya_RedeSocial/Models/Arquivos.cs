acc5208fd54f0e441a8dce828503196cbdd99692
Victor Rodrigues <victorluizrodrigues@gmail.com>
2017-11-22 15:56:45 -0200

    Terminando retoques da página perfil.

--- Symphonya_RedeSocial/Symphonya_RedeSocial/Models/Arquivos.cs
+++ Symphonya_RedeSocial/Symphonya_RedeSocial/Models/Arquivos.cs
@@ -14,8 +14,8 @@ namespace Symphonya_RedeSocial.Models
         public String Nome { get; set; }
 
         public Arquivos() { }
-        public static Boolean NovoArquivo(String tipo, String Nome, Int32 ID)
 
+        public Boolean NovoArquivo(String tipo, String Nome, int ID)
         {
 
             SqlConnection Conexao = new SqlConnection("Server=ESN509VMSSQL;Database=Symphonya;User Id=Aluno;Password=Senai1234;");
@@ -45,32 +45,22 @@ namespace Symphonya_RedeSocial.Models
             SqlCommand Comando = new SqlCommand();
             Comando.Connection = Conexao;
             if (limite)
-                Comando.CommandText = "	SELECT TOP 3 ID,Instrumento.Nome, Instrumento.Icone, Nivel FROM Instrumento, Usuario_Has_Instrumentos WHERE Instrumento.ID = InstrumentoID AND UsuarioID = @ID;";
+                Comando.CommandText = "	SELECT TOP 3 Usuario.Nome, Usuario.Sobrenome, Arquivos.Nome FROM Usuario,Arquivos WHERE Usuario_ID LIKE @ID;";
             else
-                Comando.CommandText = "	SELECT ID,Instrumento.Nome, Instrumento.Icone, Nivel FROM Instrumento, Usuario_Has_Instrumentos WHERE Instrumento.ID = InstrumentoID AND UsuarioID = @ID;";
+                Comando.CommandText = "	SELECT Usuario.Nome, Usuario.Sobrenome, Arquivos.Nome FROM Usuario,Arquivos WHERE Usuario_ID LIKE @ID;";
 
             Comando.Parameters.AddWithValue("@ID", ID);
 
             SqlDataReader Leitor = Comando.ExecuteReader();
 
-            List<Instrumentos> Instrumentos = new List<Instrumentos>();
+            List<Arquivos> Arcs = new List<Arquivos>();
             while (Leitor.Read())
             {
-                Instrumentos Instrumento = new Instrumentos();
-                Instrumento.ID = (Int32)Leitor["ID"];
-                Instrumento.Nome = (String)Leitor["Nome"];
-                Instrumento.Icone = (String)Leitor["Icone"];
-                int maestria = (Int32)Leitor["Nivel"];
-                switch (maestria)
-                {
-                    case 1: Instrumento.Maestria = 25; break;
-                    case 2: Instrumento.Maestria = 50; break;
-                    case 3: Instrumento.Maestria = 75; break;
-                    case 4: Instrumento.Maestria = 100; break;
-                    default: break;
-                }
+                Arquivos A = new Arquivos();
+                A.ID = (Int32)Leitor["ID"];
+                A.Nome = (String)Leitor["Nome"];
 
-                Instrumentos.Add(Instrumento);
+                Arcs.Add(A);
             }
 
             if (!Leitor.HasRows)
@@ -81,7 +71,7 @@ namespace Symphonya_RedeSocial.Models
 
             Conexao.Close();
 
-            return Instrumentos;
+            return Arcs;
         }
     }
 
