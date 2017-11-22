9f48c257d3cca5d22362bc6b9043b927c02fc1f4
Gabriel Alonso <gabriel_alonso14@hotmail.com>
2017-11-22 15:57:18 -0200

    Atualizando Ver BANDAS

--- Symphonya_RedeSocial/Symphonya_RedeSocial/Controllers/MenuController.cs
+++ Symphonya_RedeSocial/Symphonya_RedeSocial/Controllers/MenuController.cs
@@ -654,9 +654,9 @@ namespace Symphonya_RedeSocial.Controllers
                     List<Usuario> Usuarios = Usuario.Listar(busca);
                     ViewBag.Usuarios = Usuarios;
                 }
-                if (Bandas.Listar(busca) != null)
+                if (Bandas.ListarBandas(busca) != null)
                 {
-                    List<Bandas> Bands = Bandas.Listar(busca);
+                    List<Bandas> Bands = Bandas.ListarBandas(busca);
                     ViewBag.Bandas = Bands;
                 }
             }
@@ -803,6 +803,58 @@ namespace Symphonya_RedeSocial.Controllers
                 {
                     List<Seguidores> seguidos = Seguidores.ListarSeguidos(IDUsuario);
                     ViewBag.Seguidos = seguidos;
+                }
+                else
+                {
+                    ViewBag.Erro = "Não foram encontrados seguidores!";
+                }
+
+                //METODO PARA BUSCA DE USUARIOS, MUSICAS, BANDAS
+                if (Request.HttpMethod == "POST")
+                {
+                    String busca = Request.Form["busca"].ToString();
+                    Response.Redirect("/Menu/Pesquisar/" + busca);
+                }
+            }
+            //CASO SESSAO SEJA NULA -> REDIRECIONAMENTO PARA PAGINA LOGIN
+            else
+            {
+                Response.Redirect("/Acesso/Login");
+            }
+
+            return View();
+        }
+
+        public ActionResult VerIntegrantes()
+        {
+            //VERIFICA SE EXISTE ALGUM DADO NA SESSÃO USUARIO
+            if (Session["Usuario"] != null || Session["Administrador"] != null)
+            {
+                //CRIA VARIAVEL GLOBAL DO ID DO USUARIO
+                Int32 IDUsuario;
+
+                if (Session["Administrador"] != null)
+                {
+                    //CRIA SESSÃO DO Administrador
+                    ViewBag.Logado = Session["Administrador"];
+                    Usuario User = (Usuario)Session["Administrador"];
+                    ViewBag.User = User;
+                    IDUsuario = User.ID;
+                }
+
+                else
+                {
+                    //CRIA SESSÃO DO USUARIO
+                    ViewBag.Logado = Session["Usuario"];
+                    Usuario User = (Usuario)Session["Usuario"];
+                    ViewBag.User = User;
+                    IDUsuario = User.ID;
+                }
+
+                if (Seguidores.ListarSeguidores(IDUsuario) != null)
+                {
+                    List<Seguidores> seguidores = Seguidores.ListarSeguidores(IDUsuario);
+                    ViewBag.Seguidores = seguidores;
                 }
                 else
                 {
